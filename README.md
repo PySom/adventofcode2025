79b9a502a63b34b1b48a9865948ed69c33977cce
VNwisu <VNwisu@VNWISU-NB>
2025-12-04 16:07:05 +0100

    added solution for day 4

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ My submissions Advent of code 2025
 - Day one took me quite some time, hopeful to improve
 - Part two of day two was also challenging and the solution is inefficient. Will keep improving
 - Improved on day 3 but still slower
+- Improved on day 4 but need to think of a better solution
 
 .NET version - 10
 
---
+++ days/day_04/day_04_part_2.cs
@@ -0,0 +1,104 @@
+var input = File.ReadLines(Path.Combine(Directory.GetCurrentDirectory(), "input", "day_04.txt"));
+
+var graph = input.Select(line => line.Select(character => character).ToArray()).ToArray();
+
+var removed = InitializeGraph();
+
+bool[][] InitializeGraph() => graph.Select(line => line.Select(_ => false).ToArray()).ToArray();
+int sum = 0;
+int localMinimalSum = 0;
+// O(c*n*m)
+while (true) // O(c) total number of times a removal can occur
+{
+    for(int i = 0; i < graph.Length; i++) // O(n)
+    {
+        int maxColumn = graph[i].Length;
+        for(int j = 0; j < maxColumn; j++) // O(m)
+        {
+            if(graph[i][j] == '.') continue;
+            if(HasFewerRolls(i, j, maxColumn) < 4) // O(1)
+            {
+                removed[i][j] = true;
+                localMinimalSum++;
+
+            }
+        }
+    }
+    sum += localMinimalSum;
+    if (localMinimalSum == 0)
+    {
+        break;
+    }
+    
+    // recreate graph
+    for(int i = 0; i < graph.Length; i++) // O(n)
+    {
+        int maxColumn = graph[i].Length;
+        for(int j = 0; j < maxColumn; j++) // O(m)
+        {
+            if(removed[i][j]) // O(1)
+            {
+                graph[i][j] = '.';
+
+            }
+        }
+    }
+    removed = InitializeGraph();
+    localMinimalSum = 0;
+}
+
+Console.Write(sum);
+int HasFewerRolls(int i, int j, int maxColumn)
+{
+    int maxRow = graph.Length;
+    int localSum = 0;
+    int leftIndex = j - 1;
+    int rightIndex = j + 1;
+    int topIndex = i - 1;
+    int bottomIndex = i + 1;
+
+    int ItemCount(int row, int column) => graph[row][column] == '.' ? 0 : 1;
+
+    // top i - 1, j
+    if(topIndex >= 0)
+    {
+        localSum += ItemCount(topIndex, j);
+    }
+    // right i, j + 1
+    if(rightIndex < maxColumn)
+    {
+        localSum += ItemCount(i, rightIndex);
+    }
+    // bottom i + 1, j
+    if(bottomIndex < maxRow)
+    {
+        localSum += ItemCount(bottomIndex, j);
+    }
+    // left i, j - 1
+    if(leftIndex >= 0)
+    {
+        localSum += ItemCount(i, leftIndex);
+    }
+    // ---------------------------- //
+    // top left i - 1, j - 1
+    if(topIndex >= 0 && leftIndex >= 0)
+    {
+        localSum += ItemCount(topIndex, leftIndex);
+    }
+    // top right i - 1, j + 1
+    if(topIndex >= 0 && rightIndex < maxColumn)
+    {
+        localSum += ItemCount(topIndex, rightIndex);
+    }
+    // bottom left i + 1, j - 1
+    if(bottomIndex < maxRow && leftIndex >= 0)
+    {
+        localSum += ItemCount(bottomIndex, leftIndex);
+    }
+    // bottom right i + 1, j + 1
+    if(bottomIndex < maxRow && rightIndex < maxColumn)
+    {
+        localSum += ItemCount(bottomIndex, rightIndex);
+    }
+    return localSum;
+}
